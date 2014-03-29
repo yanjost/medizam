9b71bd5b278cf90bc3445d3fd9312a0213031dca
Adrien LAUER <adrien.lauer@gmail.com>
2014-03-29 01:03:20 +0100

    Front end hello world

---
+++ medizam/front/static/scripts/controllers/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('medizam')
+  .controller('MainCtrl', function ($scope) {
+    $scope.name = 'World';
+  });
--- medizam/medizam/urls.py
+++ medizam/medizam/urls.py
@@ -4,12 +4,22 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'medizam.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(
-        r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
-            'path': '/front/index.html',}),
-    url(r'^admin/', include(admin.site.urls)),
-)
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'medizam.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#     url(
+#         r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
+#             'path': '/front/index.html',}),
+#
+#     url(r'^admin/', include(admin.site.urls)),
+# )
+urlpatterns = patterns(
+    'django.contrib.staticfiles.views',
+    url(r'^(?:index.html)?$', 'serve', kwargs={'path': 'index.html'}),
+    url(r'^(?P<path>scripts/.*)$', 'serve'),
+    url(r'^(?P<path>styles/.*)$', 'serve'),
+    url(r'^(?P<path>views/.*)$', 'serve'),
+    url(r'^(?P<path>libs/.*)$', 'serve'),
+    url(r'^(?P<path>images/.*)$', 'serve'),
+    )
